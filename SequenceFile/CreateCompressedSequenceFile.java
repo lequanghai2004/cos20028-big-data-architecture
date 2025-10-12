bc9ea4381654f663bc978db26d541a9dc1d255e1
Le Quang Hai <hai.le.quang@accenture.com>
2025-10-13 01:07:28 +0700

    red

--- SequenceFile/CreateCompressedSequenceFile.java
+++ SequenceFile/CreateCompressedSequenceFile.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.compress.SnappyCodec;
+import org.apache.hadoop.io.SequenceFile.CompressionType;
 
 
 public class CreateCompressedSequenceFile extends Configured implements Tool {
@@ -32,6 +33,7 @@ public class CreateCompressedSequenceFile extends Configured implements Tool {
         job.setOutputFormatClass(SequenceFileOutputFormat.class);
         FileOutputFormat.setCompressOutput(job, true);
         FileOutputFormat.setOutputCompressorClass(job, SnappyCodec.class);
+        SequenceFileOutputFormat.setOutputCompressionType(job, CompressionType.BLOCK);
 
         return job.waitForCompletion(true) ? 0 : 1;
     }
