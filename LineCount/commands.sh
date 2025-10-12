24d02de2d9e9b7d6e5dc4e5e33d7df66e9e73c79
Le Quang Hai <hai.le.quang@accenture.com>
2025-10-12 16:02:21 +0700

    red

--- LineCount/commands.sh
+++ LineCount/commands.sh
@@ -1,11 +1,14 @@
+# Remove previous output directory if it exists
+hadoop fs -rm -r linecounts
+
 # Compile Java files
-javac -classpath `hadoop classpath` *.java
+javac -classpath `hadoop classpath` LineCount/*.java
 
 # Create JAR file
-jar cvf wc.jar *.class
+jar cvf wc.jar LineCount/*.class
 
 # Run the Hadoop job
-hadoop jar wc.jar Driver bible linecounts
+hadoop jar wc.jar LineCount.Driver bible.tar.gz linecounts
 
 # Display the output
 hadoop fs -cat linecounts/part-r-00000
