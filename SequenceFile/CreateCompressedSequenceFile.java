484380bb0cf941d36705de8b63b47a2690e06674
Le Quang Hai <hai.le.quang@accenture.com>
2025-10-13 00:57:59 +0700

    red

---
+++ SequenceFile/CreateCompressedSequenceFile.java
@@ -0,0 +1,44 @@
+package SequenceFile;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.io.compress.SnappyCodec;
+
+
+public class CreateCompressedSequenceFile extends Configured implements Tool {
+
+    @Override
+    public int run(String[] args) throws Exception {
+
+        if(args.length != 2) {
+            System.err.println("Usage: CreateCompressedSequenceFile <intput path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = Job.getInstance(getConf(), "Create Sequence File");
+        job.setJarByClass(CreateCompressedSequenceFile.class);
+        job.setNumReduceTasks(0);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setOutputFormatClass(SequenceFileOutputFormat.class);
+        FileOutputFormat.setCompressOutput(job, true);
+        FileOutputFormat.setOutputCompressorClass(job, SnappyCodec.class);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+     
+        int exitCode = ToolRunner.run(new Configuration(), new CreateCompressedSequenceFile(), args);
+        System.exit(exitCode);
+    }
+}
