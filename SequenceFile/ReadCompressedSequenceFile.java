138584570b97a79ebe47456002503e665d28d6ec
Le Quang Hai <hai.le.quang@accenture.com>
2025-10-13 00:50:06 +0700

    red

--- SequenceFile/ReadCompressedSequenceFile.java
+++ SequenceFile/ReadCompressedSequenceFile.java
@@ -4,10 +4,9 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.compress.SnappyCodec;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -27,10 +26,7 @@ public class ReadCompressedSequenceFile extends Configured implements Tool {
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-        job.setOutputFormatClass(SequenceFileOutputFormat.class);
-        FileOutputFormat.setCompressOutput(job, true);
-        FileOutputFormat.setOutputCompressorClass(job, SnappyCodec.class);
+        job.setInputFormatClass(SequenceFileInputFormat.class);
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
