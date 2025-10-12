e0778c16a51a7cf8afbe5cc01230bff3e858f5a1
Le Quang Hai <hai.le.quang@accenture.com>
2025-10-13 02:30:33 +0700

    red

--- ImageCount/Driver.java
+++ ImageCount/Driver.java
@@ -3,6 +3,7 @@ package ImageCount;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -29,7 +30,7 @@ public class Driver extends Configured implements Tool {
         job.setNumReduceTasks(0); // No reducer needed
 
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
--- ImageCount/ImageTypeMapper.java
+++ ImageCount/ImageTypeMapper.java
@@ -3,11 +3,17 @@ package ImageCount;
 import java.io.IOException;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class ImageTypeMapper extends Mapper<LongWritable, Text, Text, Text> {
+public class ImageTypeMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private static final Pattern p = Pattern.compile("\\.(jpg|jpeg|png|gif|webp|svg|ico|tiff|raw|cr2|nef|arw|heic|psd|indd|ai|eps)\\b");
+    private static final Text text = new Text();
+    private static final IntWritable one = new IntWritable(1);
 
     @Override
     protected void map(LongWritable key, Text value, Context context)
@@ -16,12 +22,13 @@ public class ImageTypeMapper extends Mapper<LongWritable, Text, Text, Text> {
         String line = value.toString().toLowerCase();
 
         // Regex to match any filename ending with image extension
-        Pattern p = Pattern.compile("\\.(jpg|jpeg|png|gif|webp|svg|ico|tiff|raw|cr2|nef|arw|heic|psd|indd|ai|eps)\\b");
         Matcher m = p.matcher(line);
 
         if (m.find()) {
             String imageType = m.group(1);
             context.getCounter("ImageTypes", imageType).increment(1);
+            text.set(imageType);
+            context.write(text, one);            
         }
     }
 }
