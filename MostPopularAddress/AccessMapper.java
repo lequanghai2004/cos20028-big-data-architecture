d4294f0135b5db609eb5453f978a8171c9555b2e
Le Quang Hai <hai.le.quang@accenture.com>
2025-10-14 08:41:48 +0700

    red

--- MostPopularAddress/AccessMapper.java
+++ MostPopularAddress/AccessMapper.java
@@ -6,25 +6,24 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 
-public class AddressMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
+public class AccessMapper extends Mapper<LongWritable, Text, Text, LongWritable> {
 
-    private final static LongWritable one = new LongWritable(1);
-
-    @Override
+    @Override // from offset (ip \t count) to count, ip
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
 
         // Each line in the log file is represented by 'value'
         String line = value.toString();
 
         // Split the line into parts based on spaces using regex to handle multiple spaces
-        String[] parts = line.split("\\s+");
-        
+        String[] parts = line.split("\\t+");
+
         // Check if the line has enough parts to avoid ArrayIndexOutOfBoundsException
-        if (parts.length > 0) {
+        if (parts.length > 1) {
             // The IP address is typically the first part of the log entry
             Text ipAddress = new Text(parts[0]);
+            LongWritable count = new LongWritable(Long.parseLong(parts[1]));
             // Emit the IP address with a count of one
-            context.write(ipAddress, one);
+            context.write(ipAddress, count);
         }
     }
 }
