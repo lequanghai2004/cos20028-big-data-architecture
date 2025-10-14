a5f5b0a4ef2cb47a97f545456dab623df4d74636
Le Quang Hai <hai.le.quang@accenture.com>
2025-10-14 09:20:34 +0700

    red

--- MostPopularAddress/AddressMapper.java
+++ MostPopularAddress/AddressMapper.java
@@ -25,6 +25,7 @@ public class AddressMapper extends Mapper<LongWritable, Text, Text, LongWritable
             Text ipAddress = new Text(parts[0]);
 
             // Month extraction logic
+            // Sample log format: "10.223.157.186 - - [15/Jul/2009:15:50:51 -0700] "GET / HTTP/1.1" 200 9157"
             String[] part2s = line.split("\\[\\d{2}/\\w{3}/");
             if(part2s.length > 0) {
                 String month = part2s[0].substring(part2s[0].length() - 4, part2s[0].length() - 1);
