0f63a3dc302efaa2eda10941e7cca67242737acf
Le Quang Hai <hai.le.quang@accenture.com>
2025-11-16 14:05:10 +0700

    add

--- Assignment2/Question4/query.sql
+++ Assignment2/Question4/query.sql
@@ -4,30 +4,30 @@ CREATE DATABASE indigenous;
 USE indigenous
 
 CREATE TABLE lng_id (
-    lng_code VARCHAR(255) PRIMARY KEY,
+    lng_code VARCHAR(255) PRIMARY KEY COLLATE utf8mb4_bin,
     a_lng_lat VARCHAR(255),
     a_lng_lng VARCHAR(255),
     lng_uri VARCHAR(255)
 );
 
 CREATE TABLE lng_name (
-    lng_name VARCHAR(255) PRIMARY KEY
+    lng_name VARCHAR(255) PRIMARY KEY COLLATE utf8mb4_bin
 );
 
 CREATE TABLE lng_synonym (
-    lng_synonym VARCHAR(255) PRIMARY KEY
+    lng_synonym VARCHAR(255) PRIMARY KEY COLLATE utf8mb4_bin
 );
 
 CREATE TABLE lng_thl (
-    lng_thl VARCHAR(255) PRIMARY KEY
+    lng_thl VARCHAR(255) PRIMARY KEY COLLATE utf8mb4_bin
 );
 
 CREATE TABLE lng_thp (
-    lng_thp VARCHAR(255) PRIMARY KEY
+    lng_thp VARCHAR(255) PRIMARY KEY COLLATE utf8mb4_bin
 );
 
 CREATE TABLE lng_st (
-    lng_st VARCHAR(255) PRIMARY KEY
+    lng_st VARCHAR(255) PRIMARY KEY COLLATE utf8mb4_bin
 );
 
 CREATE TABLE rel_code_name (
--- Assignment2/sqoop.sh
+++ Assignment2/sqoop.sh
@@ -1,37 +1,13 @@
-#!/bin/bash
-function sqoop_export() {
-  local table_name=$1
-  local export_dir=$2
-  local columns=$3
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table lng_id --export-dir /user/training/assignment2/austlang_rdb/lng_id --fields-terminated-by '\t' --m 1
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table lng_name --columns lng_name  --export-dir /user/training/assignment2/austlang_rdb/lng_name --fields-terminated-by '\t' --m 1
+sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table lng_synonym --columns lng_synonym  --export-dir /user/training/assignment2/austlang_rdb/lng_synonym --fields-terminated-by '\t' --m 1
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table lng_thl --columns lng_thl  --export-dir /user/training/assignment2/austlang_rdb/lng_thl --fields-terminated-by '\t' --m 1
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table lng_thp --columns lng_thp  --export-dir /user/training/assignment2/austlang_rdb/lng_thp --fields-terminated-by '\t' --m 1
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table lng_st --columns lng_st  --export-dir /user/training/assignment2/austlang_rdb/lng_st --fields-terminated-by '\t' --m 1
 
-  echo "Exporting table: $table_name to HDFS directory: $export_dir with columns: $columns"
-  
-  sqoop export \
-    --connect "jdbc:mysql://localhost/indigenous" \
-    --username training \
-    --password training \
-    --table "$table_name" \
-    --columns "$columns" \
-    --export-dir "/user/training/assignment2/austlang_rdb/$export_dir" \
-    --fields-terminated-by $'\t' \
-    --m 1 > "log_${table_name}_export.txt" 2>&1
 
-  if [ $? -eq 0 ]; then
-    echo "Export of $table_name completed successfully."
-  else
-    echo "Export of $table_name failed. Check log_${table_name}_export.txt for details."
-  fi
-}
-
-sqoop_export "lng_id" "lng_id" "lng_id"
-sqoop_export "lng_name" "lng_name" "lng_name"
-sqoop_export "lng_synonym" "lng_synonym" "lng_synonym"
-sqoop_export "lng_thl" "lng_thl" "lng_thl"
-sqoop_export "lng_thp" "lng_thp" "lng_thp"
-sqoop_export "lng_st" "lng_st" "lng_st"
-
-sqoop_export "rel_code_name" "rel_code_name" "lng_code,lng_name"
-sqoop_export "rel_code_synonym" "rel_code_synonym" "lng_code,lng_synonym"
-sqoop_export "rel_code_thl" "rel_code_thl" "lng_code,lng_thl"
-sqoop_export "rel_code_thp" "rel_code_thp" "lng_code,lng_thp"
-sqoop_export "rel_code_st" "rel_code_st" "lng_code,lng_st"
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table rel_code_name --columns lng_code,lng_name --export-dir /user/training/assignment2/austlang_rdb/rel_code_name --fields-terminated-by '\t' --m 1
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table rel_code_synonym --columns lng_code,lng_synonym --export-dir /user/training/assignment2/austlang_rdb/rel_code_synonym --fields-terminated-by '\t' --m 1
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table rel_code_thl --columns lng_code,lng_thl --export-dir /user/training/assignment2/austlang_rdb/rel_code_thl --fields-terminated-by '\t' --m 1
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table rel_code_thp --columns lng_code,lng_thp --export-dir /user/training/assignment2/austlang_rdb/rel_code_thp --fields-terminated-by '\t' --m 1
+# sqoop export --connect "jdbc:mysql://localhost/indigenous" --username training --password training --table rel_code_st --columns lng_code,lng_st --export-dir /user/training/assignment2/austlang_rdb/rel_code_st --fields-terminated-by '\t' --m 1
